a1205de5232f5f806f59ad6bc0e432e216adfa34
zhaolaiyuan <704775924@qq.com>
2022-12-01 08:56:23 +0800

    尝试连接数据库，queryById 排查到长度有问题

--- rivulet-base/src/main/java/zly/rivulet/base/convertor/ConvertorManager.java
+++ rivulet-base/src/main/java/zly/rivulet/base/convertor/ConvertorManager.java
@@ -2,12 +2,26 @@ package zly.rivulet.base.convertor;
 
 import zly.rivulet.base.definer.outerType.OriginOuterType;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConvertorManager {
 
     private final Map<String, Convertor<?, ?>> CONVERTER_MAP = new ConcurrentHashMap<>();
+
+    private static final Map<Class<?>, Class<?>> BOXCLASS_MAP = new HashMap<>();
+    static {
+        BOXCLASS_MAP.put(Boolean.TYPE, Boolean.class);
+        BOXCLASS_MAP.put(Character.TYPE, Character.class);
+        BOXCLASS_MAP.put(Byte.TYPE, Byte.class);
+        BOXCLASS_MAP.put(Short.TYPE, Short.class);
+        BOXCLASS_MAP.put(Integer.TYPE, Integer.class);
+        BOXCLASS_MAP.put(Long.TYPE, Long.class);
+        BOXCLASS_MAP.put(Float.TYPE, Float.class);
+        BOXCLASS_MAP.put(Double.TYPE, Double.class);
+        BOXCLASS_MAP.put(Void.TYPE, Void.class);
+    }
 
     public synchronized <T1, T2 extends OriginOuterType> void register(Convertor<T1, T2> convertor) {
         String key = getKey(convertor.getJavaType(), convertor.getOriginOuterType());
@@ -27,6 +41,7 @@ public class ConvertorManager {
 
 
     private static String getKey(Class<?> javaType, Class<?> outerType) {
+        javaType = BOXCLASS_MAP.getOrDefault(javaType, javaType);
         return javaType.getName() + '_' + outerType.getName();
     }
 }
--- rivulet-base/src/main/java/zly/rivulet/base/utils/MapUtils.java
+++ rivulet-base/src/main/java/zly/rivulet/base/utils/MapUtils.java
@@ -11,4 +11,5 @@ public class MapUtils {
     public static boolean isNotEmpty(Map<?, ?> map) {
         return !isEmpty(map);
     }
+
 }
